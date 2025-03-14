7fd3f6f00b9ee3593b49506359dd6bf8808e9bd3
Marin <caramelmath@proton.me>
2025-03-14 02:08:29 +0100

    Add Spring Dependencies

--- FRAME/build.gradle.kts
+++ FRAME/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
     id("java")
 }
 
@@ -12,6 +14,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 tasks.test {
