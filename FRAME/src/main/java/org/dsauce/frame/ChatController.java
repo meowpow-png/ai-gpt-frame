27276af9da45c950f9116fb11f28d8c5648169bd
Marin <caramelmath@proton.me>
2025-03-15 03:47:19 +0100

    Add missing dependencies

--- FRAME/src/main/java/org/dsauce/frame/ChatController.java
+++ FRAME/src/main/java/org/dsauce/frame/ChatController.java
@@ -1,5 +1,8 @@
 package org.dsauce.frame;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.MediaType;
