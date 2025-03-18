3b9fca296cdf4ccbe68d1d8b3c5c6803a7dfb758
Marin <caramelmath@proton.me>
2025-03-18 20:32:28 +0100

    Enable React hot reload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+      host: "0.0.0.0", // Ensures it's accessible from Docker
+      watch: {
+        usePolling: true, // Required for Docker hot reload
+      },
       proxy: {
       "/api": {
         target: "http://172.17.0.1:8080", // Works for Dockerized frontend (On Linux)
